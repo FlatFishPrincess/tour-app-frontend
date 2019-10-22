a1eca5338cdbcb31026a825d92860e8f051a7819
Jiweon Park <rkawk3142@gmail.com>
2019-10-22 15:50:46 -0700

    added fake authentication && login page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,73 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Link,
+  withRouter,
+} from 'react-router-dom';
+import Login from './components/Login/Login';
+
+export const fakeAuth = {
+  isAuthenticated: false,
+  authenticate(cb) {
+    this.isAuthenticated = true
+    setTimeout(cb, 100) // fake async
+  },
+  signOut(cb) {
+    this.isAuthenticated = false
+    setTimeout(cb, 100)
+  }
+}
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  console.log('private route?', {...rest});
+  return (
+    <Route {...rest} render={(props) => {
+      console.log('props?',props);
+    return (
+      fakeAuth.isAuthenticated === true
+      ? <Component {...props} />
+      : <Redirect to={{
+        pathname: "/login",
+        state: { from : props.location }
+      }}/>
+    )}} />
+  )
+}
+
+const AuthButton = withRouter(({ history }) => (
+  fakeAuth.isAuthenticated === true
+  ? <p>Welcome! <button onClick={() => { fakeAuth.signOut(() => history.push("/"))}} >Sign out</button></p>
+  : <p>You r not logged in</p>
+))
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        {/* <AuthButton /> */}
+        <ul>
+          <li><Link to="/public">Public</Link></li>
+          <li><Link to="/protected">Protected</Link></li>
+        </ul>
+      </div>
+      <div>
+        <Route path="/public" component={Dashboard} />
+        <Route path="/login" component={Login} />
+        <PrivateRoute path="/protected" component={Protected} />
+      </div>
+    </Router>
   );
 }
+const Protected = () => {
+  return <h2>Protected!</h2>
+}
+
+const Dashboard = () => {
+  return <h2>App!</h2>;
+}
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,9 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+// import Themes from "./theme";
 import * as serviceWorker from './serviceWorker';
+import { ThemeProvider } from '@material-ui/styles';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <ThemeProvider>
+    <App />
+  </ThemeProvider>
+  ,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
